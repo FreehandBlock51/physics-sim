bab7579fd8cee50644f4d983432618b07d35938e
FreehandBlock51 <freehandblock51@outlook.com>
2025-02-20 19:25:49 -0800

    fixed matrix multiplication

--- src/common/mat4x4.c
+++ src/common/mat4x4.c
@@ -3,12 +3,19 @@
 #include <stdio.h>
 
 void mat4x4_times_mat4x4(mat4x4_t dest, mat4x4_t a, mat4x4_t b) {
+    mat4x4_t result = {};
     for (size_t row = 0; row < 4; row++) {
         for (size_t column = 0; column < 4; column++) {
-            dest[row][column] = 0;
+            phy_real_t tmp = 0;
             for (size_t common = 0; common < 4; common++) {
-                dest[row][column] += a[common][column] * b[row][common];
+                tmp += a[common][column] * b[row][common];
             }
+            result[row][column] = tmp;
+        }
+    }
+    for (size_t row = 0; row < 4; row++) {
+        for (size_t column = 0; column < 4; column++) {
+            dest[row][column] = result[row][column];
         }
     }
 }
