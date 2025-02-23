3313d89a45483cc6e6b14817f4732bc2a4bbcfac
FreehandBlock51 <freehandblock51@outlook.com>
2025-02-23 11:32:27 -0800

    added null checks to vec3.c

--- src/common/vec3.c
+++ src/common/vec3.c
@@ -1,21 +1,34 @@
 #include "common/vec3.h"
 
+#include <assert.h>
 #include <stddef.h>
 #include <math.h>
 
 void vec3_add_to(vec3_t *dest, vec3_t source, phy_real_t factor) {
+    assert(dest != NULL); // so that nullptr crashes in debug
+    if (dest == NULL) {
+        return; // so that nullptr is ignored in release
+    }
     dest->x += source.x * factor;
     dest->y += source.y * factor;
     dest->z += source.z * factor;
 }
 
 void vec3_multiply_by(vec3_t *dest, phy_real_t factor) {
+    assert(dest != NULL);
+    if (dest == NULL) {
+        return;
+    }
     dest->x *= factor;
     dest->y *= factor;
     dest->z *= factor;
 }
 
 void vec3_clear(vec3_t *vec) {
+    assert(vec != NULL);
+    if (vec == NULL) {
+        return;
+    }
     vec->x = 0;
     vec->y = 0;
     vec->z = 0;
@@ -58,11 +71,19 @@ phy_real_t vec3_magnitude(vec3_t vec) {
 }
 
 void vec3_unit(vec3_t *vec) {
+    assert(vec != NULL);
+    if (vec == NULL) {
+        return;
+    }
     phy_real_t magnitude = vec3_magnitude(*vec);
     vec3_multiply_by(vec, 1.0/magnitude);
 }
 
 void vec3_rotate_x(vec3_t *vec, phy_real_t xrot) {
+    assert(vec != NULL);
+    if (vec == NULL) {
+        return;
+    }
     vec3_t copy = *vec;
     vec->x = copy.x * 1 + copy.y * 0 + copy.z * 0;
     vec->y = copy.x * 0 + copy.y * cos(xrot) + copy.z * sin(xrot);
@@ -77,6 +98,10 @@ void vec3_rotate_y(vec3_t *vec, phy_real_t yrot) {
 }
 
 void vec3_rotate_z(vec3_t *vec, phy_real_t zrot) {
+    assert(vec != NULL);
+    if (vec == NULL) {
+        return;
+    }
     vec3_t copy = *vec;
     vec->x = copy.x * cos(zrot) + copy.y * sin(zrot) + copy.z * 0;
     vec->y = copy.x * -sin(zrot) + copy.y * cos(zrot) + copy.z * 0;
@@ -84,12 +109,20 @@ void vec3_rotate_z(vec3_t *vec, phy_real_t zrot) {
 }
 
 void vec3_rotate(vec3_t *vec, phy_real_t xrot, phy_real_t yrot, phy_real_t zrot) {
+    assert(vec != NULL);
+    if (vec == NULL) {
+        return;
+    }
     vec3_rotate_z(vec, zrot);
     vec3_rotate_y(vec, yrot);
     vec3_rotate_x(vec, xrot);
 }
 
 void vec3_cross_product(vec3_t *dest, vec3_t a, vec3_t b) {
+    assert(dest != NULL);
+    if (dest == NULL) {
+        return;
+    }
     dest->x = (a.y * b.z) - (a.z * b.y);
     dest->y = (a.z * b.x) - (a.x * b.z);
     dest->z = (a.x * b.y) - (a.y * b.x);
@@ -110,6 +143,10 @@ phy_real_t vec3_dot_product(vec3_t a, vec3_t b) {
  * be (0,2,0)
  */
 void vec3_get_portion_in_direction(vec3_t *result, vec3_t original, vec3_t direction) {
+    assert(result != NULL);
+    if (result == NULL) {
+        return;
+    }
     // get unit vectors
     vec3_t orig_unit = original;
     vec3_unit(&orig_unit);
