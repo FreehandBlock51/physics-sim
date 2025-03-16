ce9945150fb328c9fd4364cbf2def6b13f6567c3
FreehandBlock51 <freehandblock51@outlook.com>
2025-03-15 18:15:23 -0700

    Added gravity to graphical simulation

--- src/main/graphicmain.c
+++ src/main/graphicmain.c
@@ -25,21 +25,34 @@ const color_t WINDOW_BACKGROUND_COLOR = COLOR_PURPLE;
 #include "shaders/matrix_vertex.h"
 #include "shaders/uniform_color_fragment.h"
 
-static GLfloat vertices[BBOX_VERTEX_ARRAY_SIZE];
-static GLuint  indices[BBOX_INDEX_ARRAY_SIZE];
+static GLfloat box1_vertices[BBOX_VERTEX_ARRAY_SIZE];
+static GLuint  box1_indices[BBOX_INDEX_ARRAY_SIZE];
+static GLfloat box2_vertices[BBOX_VERTEX_ARRAY_SIZE];
+static GLuint  box2_indices[BBOX_INDEX_ARRAY_SIZE];
+
+#define CAMERA_MOVE_SPEED 0.05
+#define CAMERA_ROTATE_SPEED 0.0025
+
+#define PHY_FRAMES_PER_STEP 1
 
 int graphic_main(void) {
+    vec3 GLOBAL_UP = { 0, 1, 0 };
+
+
     int result;
     window_t *window = calloc(1, sizeof(window_t));
     if ((result = window_init(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, window))) {
         return result;
     }
 
-    body_t body;
-    body_make(&body, vec3_make(0, 0, 0), VEC3_ZERO, VEC3_ZERO, VEC3_ZERO, 1, 0, 0);
-    bbox_t box;
-    bbox_make(&box, 0, 0, 0, 10, 10, 10);
-    bbox_gen_vertices(box, vertices, indices);
+    body_t body1, body2;
+    body_make(&body1, vec3_make(0, 0, 0), VEC3_ZERO, VEC3_UP, VEC3_ZERO, 1, 0, 0);
+    body_make(&body2, vec3_make(-1, 1, 7), VEC3_ZERO, VEC3_DOWN, VEC3_ZERO, 1, 0, 0);
+    bbox_t box1, box2;
+    bbox_make(&box1, 0, 0, 0, 10, 10, 10);
+    bbox_make(&box2, 0, 0, 0, 5, 5, 5);
+    bbox_gen_vertices(box1, box1_vertices, box1_indices);
+    bbox_gen_vertices(box2, box2_vertices, box2_indices);
 
     l_printf("Building shaders...\n");
 
@@ -80,11 +93,13 @@ int graphic_main(void) {
 
     l_printf("Shaders successfully built!\n");
 
-    model_t box_model = model_from_indices(vertices, 3, BBOX_VERTEX_ARRAY_SIZE, MODEL_BUFFER_STATIC, indices, BBOX_INDEX_ARRAY_SIZE, MODEL_BUFFER_STATIC, MODEL_DRAW_TRIANGLES);
+    model_t box1_model = model_from_indices(box1_vertices, 3, BBOX_VERTEX_ARRAY_SIZE, MODEL_BUFFER_STATIC, box1_indices, BBOX_INDEX_ARRAY_SIZE, MODEL_BUFFER_STATIC, MODEL_DRAW_TRIANGLES);
+    model_t box2_model = model_from_indices(box2_vertices, 3, BBOX_VERTEX_ARRAY_SIZE, MODEL_BUFFER_STATIC, box2_indices, BBOX_INDEX_ARRAY_SIZE, MODEL_BUFFER_STATIC, MODEL_DRAW_TRIANGLES);
 
-    color_t color = COLOR_GREEN;
+    color_t color1 = COLOR_GREEN;
+    color_t color2 = COLOR_BLUE;
 
-    mat4 model; // generated each frame
+    mat4 model1, model2; // generated each frame
 
     camera_t camera;
     camera_make(&camera, (vec3){ 0, 0, 25 }, (vec3){ 0, 0, -1 }, (vec3){ 0, 1, 0 }, GLM_PI_4f, 1.0f, 1.0f, 0.1f, 100.0f);
@@ -93,40 +108,90 @@ int graphic_main(void) {
 
     mat4 projection;
 
+    uint32_t frames_since_last_phy_step = 0;
+
     while (!window_should_close(*window))
     {
         window_clear(WINDOW_BACKGROUND_COLOR);
 
-        // modify transformations
-        if (window_is_key_down(window, GLFW_KEY_R)) {
-            body.rotation.x += 0.01;
-            body.rotation.z += 0.01;
+        if (++frames_since_last_phy_step >= PHY_FRAMES_PER_STEP) {
+            phy_body_add_gravity_force(&body1, &body2);
+
+            phy_body_step(&body1);
+            phy_body_step(&body2);
+
+            frames_since_last_phy_step = 0;
         }
-        if (window_is_key_down(window, GLFW_KEY_UP)) {
-            body.position.y += 0.01;
+
+        box1.position = body1.position;
+        box2.position = body2.position;
+
+        // move camera up and down
+        if (window_is_key_down(window, GLFW_KEY_E)) {
+            glm_vec3_muladds(camera.up, CAMERA_MOVE_SPEED, camera.position);
         }
-        if (window_is_key_down(window, GLFW_KEY_DOWN)) {
-            body.position.y -= 0.01;
+        if (window_is_key_down(window, GLFW_KEY_Q)) {
+            glm_vec3_muladds(camera.up, -CAMERA_MOVE_SPEED, camera.position);
+        }
+
+        // move camera left and right
+        if (window_is_key_down(window, GLFW_KEY_D)) {
+            vec3 right;
+            glm_vec3_cross(camera.forward, camera.up, right);
+            glm_vec3_muladds(right, CAMERA_MOVE_SPEED, camera.position);
+        }
+        if (window_is_key_down(window, GLFW_KEY_A)) {
+            vec3 right;
+            glm_vec3_cross(camera.forward, camera.up, right);
+            glm_vec3_muladds(right, -CAMERA_MOVE_SPEED, camera.position);
+        }
+
+        // move camera forward and backward
+        if (window_is_key_down(window, GLFW_KEY_S)) {
+            glm_vec3_muladds(camera.forward, -CAMERA_MOVE_SPEED, camera.position);
+        }
+        if (window_is_key_down(window, GLFW_KEY_W)) {
+            glm_vec3_muladds(camera.forward, CAMERA_MOVE_SPEED, camera.position);
+        }
+
+        // Rotations must be applied to both the camera's forward and
+        // up to maintain the invariant that the two vectors must be
+        // purpendicular.  Use global directions so rotation doesn't
+        // stack
+
+        // rotate camera left and right
+        if (window_is_key_down(window, GLFW_KEY_LEFT)) {
+            glm_vec3_rotate(camera.forward, CAMERA_ROTATE_SPEED, GLOBAL_UP);
+            glm_vec3_rotate(camera.up, CAMERA_ROTATE_SPEED, GLOBAL_UP);
         }
         if (window_is_key_down(window, GLFW_KEY_RIGHT)) {
-            body.position.x += 0.01;
+            glm_vec3_rotate(camera.forward, -CAMERA_ROTATE_SPEED, GLOBAL_UP);
+            glm_vec3_rotate(camera.up, -CAMERA_ROTATE_SPEED, GLOBAL_UP);
         }
-        if (window_is_key_down(window, GLFW_KEY_LEFT)) {
-            body.position.x -= 0.01;
+
+        // rotate camera up and down.
+        if (window_is_key_down(window, GLFW_KEY_UP)) {
+            vec3 right;
+            glm_vec3_cross(camera.forward, camera.up, right);
+            glm_vec3_rotate(camera.forward, 0.001, right);
+            glm_vec3_rotate(camera.up, 0.001, right);
         }
-        if (window_is_key_down(window, GLFW_KEY_RIGHT_BRACKET)) {
-            body.position.z += 0.01;
+        if (window_is_key_down(window, GLFW_KEY_DOWN)) {
+            vec3 right;
+            glm_vec3_cross(camera.forward, camera.up, right);
+            glm_vec3_rotate(camera.forward, -0.001, right);
+            glm_vec3_rotate(camera.up, -0.001, right);
         }
-        if (window_is_key_down(window, GLFW_KEY_LEFT_BRACKET)) {
-            body.position.z -= 0.01;
-        }
+
+        // close the window if asked to
         if (window_is_key_released(window, GLFW_KEY_ESCAPE)) {
             l_printf("window closing...\n");
             window_close(*window);
         }
 
-        // generate transformation matrix
-        body_gen_transform(&body, model);
+        // generate transformation matrices
+        body_gen_transform(&body1, model1);
+        body_gen_transform(&body2, model2);
 
         camera_gen_view_matrix(camera, view);
         camera_gen_projection_matrix(camera, projection);
@@ -134,11 +199,20 @@ int graphic_main(void) {
         // set VAO and shader, then draw
         shader_program_use(shader_program);
         // cast to float* because we know that a matrix works here
-        glUniformMatrix4fv(u_model, 1, GL_FALSE, (float*)model);
+        glUniformMatrix4fv(u_model, 1, GL_FALSE, (float*)model1);
         glUniformMatrix4fv(u_view, 1, GL_FALSE, (float*)view);
         glUniformMatrix4fv(u_projection, 1, GL_FALSE, (float*)projection);
-        glUniform4f(u_color, color.r, color.g, color.b, color.a);
-        model_draw(box_model);
+        glUniform4f(u_color, color1.r, color1.g, color1.b, color1.a);
+        model_draw(box1_model);
+
+        // ...and do it again for the second box
+        shader_program_use(shader_program);
+        // cast to float* because we know that a matrix works here
+        glUniformMatrix4fv(u_model, 1, GL_FALSE, (float*)model2);
+        glUniformMatrix4fv(u_view, 1, GL_FALSE, (float*)view);
+        glUniformMatrix4fv(u_projection, 1, GL_FALSE, (float*)projection);
+        glUniform4f(u_color, color2.r, color2.g, color2.b, color2.a);
+        model_draw(box2_model);
 
         window_end_drawing(window);
     }
